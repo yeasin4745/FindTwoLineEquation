7e30c802b497a605a09be3ef199abd910049b4f5
MD Yeasin Ali <yeasinali4745@gmail.com>
2025-06-30 06:38:13 +0600

    Update two-point-equation.js add new code #1

--- find-line-equation.js
+++ find-line-equation.js
@@ -1,24 +1,65 @@
 (function(){
-   function findTwoLineEquation(point1=[],point2=[]) {
-   if(point1.length!== point2.length)return 'Please , Enter a valid input';
-   
-   let [x1,y1]=point1,[x2,y2]=point2;
-   let a=y1-y2,b=x2-x1;
-   let c=(x1*y2)-(x2*y1);
-     // Format the equation in standard form: ax + by + c = 0
-   const equation=`${a}x + ${b}y + ${c}=0`;
-   
-   // calculate slope 
-   const m=(y2-y1)/(x2-x1);
-   console.log('Slop: ',m)
-   return equation;
-   }
-   
-   // Example input 
-   let point1=[3,-4];
-   let point2=[4,-5];
-   console.log('Equation is :' ,findTwoLineEquation(point1,point2))
-   
-   
+   function twoPointEquation(p1 = [], p2 = []) {
+ let [x1, y1] = p1;
+ let [x2, y2] = p2;
+ 
+ // formula => x(y₁ - y₂) - y(x₁ - x₂) = x₂y₁ - x₁y₂
+ 
+ const A = y1 - y2;
+ const B = x1 - x2;
+ 
+ // correct constant term: x1*y2 - x2*y1
+ const constant = x1 * y2 - x2 * y1;
+ 
+ // formatting 
+ // equation => Ax+By=constant
+ let equation = '';
+ if (A === 0 && B === 0) {
+  equation = "0 = 0"; // same point
+ } else {
+  equation =
+   (A !== 0 ? `${A}${B > 0 ? 'x - ' : 'x + '}` : '') +
+   (B !== 0 ? `${Math.abs(B)}y` : '') +
+   ` = ${constant}`;
+ }
+ 
+ // if ( x1 == x2 , then slope =undefined)
+ const slope = (x1 === x2) ?
+  undefined :
+  (y2 - y1) / (x2 - x1);
+ 
+ return { equation, slope };
+}
+
+//console.log(twoPointEquation([4, 1], [1, 10]));
+
+
+
+// one point and slope
+function equation(point = [], m) {
+ let [x1, y1] = point;
+ 
+
+ // formula => y=mx-(mx1-y1)
+ let constant=m*x1-y1;
+ let equation='';
+ if(m==0){
+  equation=`y=${constant >=1? ' - '+constant:  Math.abs(constant)}`
+ } else {
+  equation=`y=${m}x ${constant > 0 ? '- '+ constant: '+ '+ Math.abs(constant)}`
+ }
+ return equation ;
+}
+
+//console.log(equation([2, 4], 0))
+
+
+
+
+
+
+
+
+
     
 })();
